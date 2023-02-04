f4a90dacf31466c1054b6c278ee07cf5a99066e2
KubiBatuOner <kbatuhanoner@yahoo.com>
2023-02-05 01:32:18 +0300

    S6 Challenge Tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,18 +1,82 @@
-import React from 'react';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useState, useEffect } from "react";
+import Karakter from "./components/Karakter";
+import Arama from "./Arama";
+import axios from "axios";
+import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  const [data, setData] = useState([]);
+  const [arama, setArama] = useState("");
+  const [icerik, setIcerik] = useState("");
+  const [page, setPage] = useState("1");
+  const [pages, setPages] = useState([]);
+
+  const totalPages = [];
+  function pageSetter(page) {
+    setPage(page);
+  }
+
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people?page=" + page)
+      .then((response) => {
+        setData(response.data.results);
+        let totalPage = response.data.count / response.data.results.length;
+        for (let i = 1; i <= totalPage; i++) {
+          totalPages.push(i);
+        }
+        setPages(totalPages);
+      })
+      .catch((err) => console.log(err));
+  }, [page]);
+
+  function handleClick(name) {
+    setIcerik(name === icerik ? null : name);
+  }
 
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+      {<Arama setArama={setArama} arama={arama} />}
+      {data
+        .filter((person) => {
+          if (arama === "") {
+            return person;
+          } else if (
+            person.name.toLowerCase().includes(arama.toLocaleLowerCase())
+          ) {
+            return person;
+          }
+        })
+        .map((person) => {
+          return (
+            <Karakter
+              key={person.name}
+              data={person}
+              handleClick={handleClick}
+              icerik={icerik}
+              setIcerik={setIcerik}
+            />
+          );
+        })}
+      <Pagination style={{ justifyContent: "center" }}>
+        {pages.map((item) => (
+          <PaginationItem onClick={() => pageSetter(item)}>
+            <PaginationLink>{item}</PaginationLink>
+          </PaginationItem>
+        ))}
+      </Pagination>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,41 @@
 // Karakter bileşeniniz buraya gelecek
+import React from "react";
+import styled from "styled-components";
+
+function Karakter(props) {
+  const { data, handleClick, icerik } = props;
+
+  const SCInfo = styled.div`
+    ${(props) =>
+      props.icerik !== props.dataName ? `display:none` : `display:block`}
+  `;
+
+  const SCKarakter = styled.div`
+    width: 40%;
+    margin: 10px auto;
+    background-color: rgba(20, 0, 0, 0.5);
+    color: white;
+    padding: 3px 0 14px 0px;
+  `;
+
+  return (
+    <SCKarakter>
+      <h3 key={data.name}>{data.name}</h3>
+      <button onClick={() => handleClick(data.name)}>
+        {icerik !== data.name ? "+" : "-"}
+      </button>
+      <SCInfo icerik={icerik} dataName={data.name}>
+        <p>Gender: {data.gender}</p>
+        <p>Height: {data.height}</p>
+        <p>Mass: {data.mass}</p>
+        <p>BirthYear: {data.birth_year}</p>
+        <p>Eye Color: {data.eye_color}</p>
+        <p>Hair Color: {data.hair_color}</p>
+        <p>Skin Color: {data.skin_color}</p>
+        {<p>Appears in {data.films.length} films</p>}
+      </SCInfo>
+    </SCKarakter>
+  );
+}
+
+export default Karakter;
